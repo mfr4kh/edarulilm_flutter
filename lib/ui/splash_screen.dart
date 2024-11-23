02d4160197e40c611fb7c59ee82c00346f2ac134
Fazlur Rahman <rahmanfazlur329@gmail.com>
2024-11-24 03:00:22 +0530

    1st commit.

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
-import 'home_page.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Simulating a check for isLoggedIn (this should come from shared preferences or state management)
-    bool isLoggedIn = false; // Replace with actual logic
-
-    Future.delayed(Duration(seconds: 2), () {
-      if (isLoggedIn) {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage(title: '',)));
-      } else {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
-      }
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginPage()
+        ),
+      );
     });
 
     return Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+      body: Center(
+        child: Text(
+          'Welcome to Video App',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
